8cc03d25a69244873879746980117fcdfea90a67
Maxime <maximeringeval@orange.fr>
2022-04-07 15:59:42 +0200

    todos

--- API/controllers/todos.js
+++ API/controllers/todos.js
@@ -8,5 +8,18 @@ exports.create_todos = (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
     const userID = decodedToken.userID;
 
+
+    .then((todos) => {
+      const newtodos = new Todos({
+        name: req.body.name,
+        content: req.body.content,
+        userID: req.body.userid
+      });
+      newtodos
+        .save()
+        .then(() => res.status(201).json({ message: "La liste viens d'être créé" }))
+        .catch((err) => res.status(401).json({ err }));
+    })
+    .catch((err) => res.status(500).json({ err }));
 }
 
