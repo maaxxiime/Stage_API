cb676fcd843eb3df181aa1a390d5f6e0ae9cbc34
Maxime <maximeringeval@orange.fr>
2022-04-07 11:32:07 +0200

    route_todos

--- API/routes/todos.js
+++ API/routes/todos.js
@@ -1,12 +1,12 @@
 const express = require('Express')
 const router = express.Router();
 
-// router.post('/signup', controllers.signup);
-// route get all
-// route get id
-// route get userid (tout les todos)
-// router.delete('/:TargetId', auth, controllers.delete);
-// router.put('/:TargetId', auth, controllers.update);
+router.post('todos/', auth, controllers.create_todos);
+router.get('/todos/:TagerId', controllers.read_one);
+router.get('/todos/', controllers.read_all);
+router.put('todos/:TargetId', auth, controllers.update_todos);
+router.delete('todos/:TargetId', auth, controllers.delete_todos);
+
 
 
 // const auth = require('../middlewares/auth.js')
